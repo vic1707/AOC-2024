b3427009711daf8b219ebbf91b638b4834cd915d
vic1707 <28602203+vic1707@users.noreply.github.com>
2024-12-02 14:21:41 +0100

    init d1

---
+++ src/bin/01.rs
@@ -0,0 +1,38 @@
+advent_of_code::solution!(1);
+
+pub fn part_one(input: &str) -> Option<usize> {
+    None
+}
+
+pub fn part_two(input: &str) -> Option<usize> {
+    None
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_example_part_one() {
+        let result = part_one(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, None);
+    }
+
+    #[test]
+    fn test_part_one() {
+        let result = part_one(&advent_of_code::template::read_file("inputs", DAY));
+        assert_eq!(result, None);
+    }
+
+    #[test]
+    fn test_example_part_two() {
+        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, None);
+    }
+
+    #[test]
+    fn test_part_two() {
+        let result = part_two(&advent_of_code::template::read_file("inputs", DAY));
+        assert_eq!(result, None);
+    }
+}
